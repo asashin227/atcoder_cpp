019440b050bee6a299501b968a3d6d2dc0b6beee
asashin227 <asa.shin.asa@gmail.com>
2019-09-27 21:43:13 +0900

    clear ABC081A

--- abc081_a/main.cpp
+++ abc081_a/main.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
+#include <string.h>
 using namespace std;
 int main()
 {
-    char *str = (char *)malloc(3);
+    char *str = (char *)malloc(2);
     int cnt;
     cin >> str;
     for(int i = 0; i < 3; i++) {
-        if (&str[i] == "1") {
+        if (strncmp("1",&str[i],1)==0) {
             cnt++;
         }
     }

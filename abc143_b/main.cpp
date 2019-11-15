d5d0ba6bfb6786a226bf175bbed162bddcaaf72c
asashin227 <asa.shin.asa@gmail.com>
2019-11-15 20:06:12 +0900

    print input values

--- abc143_b/main.cpp
+++ abc143_b/main.cpp
@@ -1,9 +1,6 @@
-#include <iostream>
-#include <numeric>
-
+#include<iostream>
 using namespace std;
-
-
+// https://atcoder.jp/contests/abc143/tasks/abc143_b
 // 問題文
 // たこ焼きフェスティバル (たこフェス) の季節がやってきました！
 
@@ -11,16 +8,15 @@ using namespace std;
 // ところで、おいしさが x と y であるたこ焼きを一緒に食べると、体力が x × y 回復することが一般に知られています。
 // たこフェスでふるまわれる N 個のたこ焼きから、2 個を選ぶ方法はN×(N−1) / 2通り考えられます。そのそれぞれについて、一緒に食べたときの体力の回復量を求めて、その総和を出力してください。
 
-int main()
-{
-    int n;
-    cin >> n;
+int main() {
+    int N;
+    cin >> N;
+    int d[N];
+    for(int i = 0; i < N; i++) {
+        cin >> d[i];
+    }
 
-    char *str = (char *)malloc(n);
-    cin >> str;
-
-    const size_t size = sizeof(str) / sizeof(str[0]);
-    int sum = std::accumulate(str, str + size, 0);
-    cout << sum;
-    return 0;
+    for (int i = 0; i < N; i++) {
+        cout << d[i] << endl;
+    }
 }
